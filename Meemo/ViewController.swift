cad3a05bf2809b1e5a27551a010f095d038c0ac4
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2017-04-22 13:32:34 +0200

    Basic video stream from firebase

--- Meemo/ViewController.swift
+++ Meemo/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
+    
+    var playerController = AVPlayerViewController()
+    var player:AVPlayer?
+    
+    let testURL = "https://firebasestorage.googleapis.com/v0/b/meemo-external-test.appspot.com/o/Video.mp4?alt=media&token=88591412-30a1-4bdf-af82-6bffef5268d2"
 
+    @IBAction func playVideo(_ sender: AnyObject) {
+        self.present(self.playerController, animated: true, completion:{
+            self.playerController.player?.play()
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let videoString:String? = testURL
+    
+        if let url = videoString {
+            let videoURL = NSURL(string: url)
+            self.player = AVPlayer(url: videoURL as! URL)
+            self.playerController.player = self.player
+        }
     }
 
     override func didReceiveMemoryWarning() {
